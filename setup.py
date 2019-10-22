51c71fe078f3b0b33c81a60dda93469561099872
Niarfe <efrain.olivares@gmail.com>
2019-10-21 21:12:44 -0700

    v0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='theseus',
-    version='0.0.4',
+    version='0.0.5',
     description='Graphical frequency neuron like node',
     url='https://github.com/Niarfe/theseus_node.git',
     author='Efrain Olivares',
